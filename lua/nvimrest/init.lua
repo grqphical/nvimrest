22bb1c72812608aacaa92461e4ba8e05336fb6fd
Nathan Jacobson <95062977+grqphical@users.noreply.github.com>
2025-07-28 22:29:58 -0700

    fix: 🛠️ created setup function

--- lua/nvimrest/init.lua
+++ lua/nvimrest/init.lua
@@ -1,5 +1,11 @@
 local editor = require("editor")
 
-vim.api.nvim_create_user_command("NvimRest", function()
-    editor:create_http_editor()
-end, {})
+local M = {}
+
+function M:setup()
+    vim.api.nvim_create_user_command("NvimRest", function()
+        editor:create_http_editor()
+    end, {})
+end
+
+return M
