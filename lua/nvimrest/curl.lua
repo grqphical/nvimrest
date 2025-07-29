d4ea26b5b8a007ad6d8aadb9e58751094d821b2f
Nathan Jacobson <95062977+grqphical@users.noreply.github.com>
2025-07-28 23:09:03 -0700

    refactor: 🔨 changed to Nvim async API

--- lua/nvimrest/curl.lua
+++ lua/nvimrest/curl.lua
@@ -1,3 +1,11 @@
+local function split(str, delimiter)
+    local result = {}
+    for part in string.gmatch(str, "[^" .. delimiter .. "]+") do
+        table.insert(result, part)
+    end
+    return result
+end
+
 -- make sure the user has curl installed
 local curl_process_result = vim.system({ 'curl', '--version' }):wait(500)
 
@@ -19,8 +27,13 @@ function M:do_request(request)
 
     cmd = cmd .. " " .. request.url
 
-    local curl_result = vim.fn.system(cmd)
-    print(curl_result)
+    vim.system(split(cmd), { text = true }, function(out)
+        if out.code ~= 0 then
+            error(string.format("error while running HTTP request (curl exit code: %d): %s", out.code, out.stderr))
+        end
+
+        print(out.stdout)
+    end)
 end
 
 return M
